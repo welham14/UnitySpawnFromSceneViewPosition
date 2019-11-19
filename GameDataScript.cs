335120124942f103b5f3faa7db6dfd08d099edf0
Ryan Welham <rtxw@live.com>
2019-11-18 23:42:22 -0500

    Add files via upload

--- GameDataScript.cs
+++ GameDataScript.cs
@@ -5,8 +5,10 @@ public class GameDataScript : MonoBehaviour{
 
     static public Vector3 playerStartPos;
     public Transform player;
+    public bool autoUpdatePosition = true;
 
     private void Start() {
+        if (autoUpdatePosition)
         player.position = playerStartPos;
     }
 }
